c0ff9ea29f0d48554664e24444a4c06e71963e49
218252005 <typhigov20@gmail.com>
2021-05-16 22:14:21 +0200

    commit 2

--- src/test/java/za/ac/cput/ProjectTest.java
+++ src/test/java/za/ac/cput/ProjectTest.java
@@ -1,10 +1,87 @@
 package za.ac.cput;
 
 import java.util.*;
-import org.junit.Test;
 
 public class ProjectTest
 {
+    public static void main(String args[])
+    {
+
+        //List
+        List<String> student1 = new ArrayList<>();
+        List<String> student2 = new ArrayList<>();
+
+        //add
+        student1.add("Tyreeq");
+        student1.add("Caelum");
+
+        student2.add("Tyreeq");
+
+        //remove
+        student1.remove("Caelum");
+
+        System.out.println("List:");
+        System.out.println(student1);
+        System.out.println(student2);
+
+        //find
+        if(student1.contains("Tyreeq"))
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+
+        //Set
+        Set<String> food1 = new HashSet<>();
+
+        //add
+        food1.add("Pizza");
+        food1.add("Pie");
+
+        //remove
+        food1.remove("Pizza");
+
+        System.out.println("Set:");
+        System.out.println(food1);
+
+        //find
+        if(food1.contains("Pizza"))
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+
+        //Map
+        Map<String,Integer> gameOrder = new HashMap<>();
+
+        //add
+        gameOrder.put("Bloodborne",0);
+        gameOrder.put("God of War", 1);
+
+        //remove
+        gameOrder.remove("God of War", 1);
+
+        System.out.println("Map:");
+        System.out.println(gameOrder);
+
+        //find
+        if(gameOrder.containsValue(1))
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+
+        //Collection
 
 
+    }
 }
